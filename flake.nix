09c59ba7be02e82b69ac109449ab7c7c8240e617
hugosenari <hugosenari@gmail.com>
2022-03-12 02:11:14 -0300

    chore: re index wxpay

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A wechat payment sdk for nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."wxpay-main".url = "path:./main";
+  inputs."wxpay-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."wxpay-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
