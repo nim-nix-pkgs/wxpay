fd93cadf1589d9f9bd52c94d3caf17c7b45321c4
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:59 -0300

    chore: re index wxpay

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."wxpay-main".dir   = "main";
+  inputs."wxpay-main".owner = "nim-nix-pkgs";
+  inputs."wxpay-main".ref   = "master";
+  inputs."wxpay-main".repo  = "wxpay";
+  inputs."wxpay-main".type  = "github";
+  inputs."wxpay-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."wxpay-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
